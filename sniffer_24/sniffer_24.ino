f422c873d478570244bd22d471b38dd90f25d89b
Ronan Gaillard <ronan.gaillard@live.fr>
2016-12-30 19:30:32 +0100

    Update sniffing

--- sniffer_24/sniffer_24.ino
+++ sniffer_24/sniffer_24.ino
@@ -1,18 +1,23 @@
 #include "nrf24.h"
 #include "logitech-device.h"
 
+uint8_t base_mac_address[] = {0xBB, 0x0A, 0xDC, 0xA5, 0x75};
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting...");
 
   init_nrf24();
+  nrf24_set_bandwith(2); /* Logitech keyboards use 2MHz bw */
 
   uint8_t channel = read_register(REG_RF_CH);
 
   Serial.print("Channel should be : ");
-  Serial.println(INIT_CHANNEL_MAPPED, DEC);
+  Serial.println(INIT_CHANNEL, DEC);
   Serial.print("Channel is : ");
-  Serial.println(channel, DEC);
+  Serial.println(channel + 2400, DEC);
+
+  nrf24_set_rx_address(base_mac_address, 5);
 }
 
 void loop() {
@@ -25,10 +30,13 @@ void loop() {
 
       nrf24_set_channel(channel);
 
-      delay(1000);
+      delay(30000);
 
       if(!nrf24_rx_fifo_empty())
         Serial.println("Heard something !!");
   }
+
+  Serial.println("DONE ! RESET TO RESTART !");
+  while(1) {}
 
 }
